0aa18e9ba1d875fcd9509864de1565084fe95448
Kanatbek-AKA <utrechtenov@gmail.com>
2023-03-11 21:47:11 +0000

    2 week done

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -5,20 +5,15 @@ from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
-    # route is a string contains a URL pattern
-    # view refers to the view function
-    # name the URL
 
     # path for about view
     path("about_us/", views.about, name="about_us"),
     # path for contact us view
     path("djangoapp/contact_us/", views.contact, name="contact_us"),
-    # path for registration
-    # path("registration/", views.registration, name="registration"),
-    # path for login
-    # path("login/", views.login, name="login"),
-    # path for logout
-    # path("logout/", views.logout, name="logout"),
+    # path for registration, login, logout
+    path('registration/', views.registration_request, name='registration'),
+    path('login/', views.login_request, name='login'),
+    path('logout/', views.logout_request, name='logout'),
     # path  
     path(route='', view=views.get_dealerships, name='index'),
 
