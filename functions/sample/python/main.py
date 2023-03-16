095b6e121e03e08287a46a6d7e16f12f79f1cd63
Kanatbek-AKA <61031156+Kanatbek-AKA@users.noreply.github.com>
2023-03-16 17:18:11 +0100

    Update main.py

--- functions/sample/python/main.py
+++ functions/sample/python/main.py
@@ -3,33 +3,44 @@
 Returns:
     List: List of reviews for the given dealership
 """
-from cloudant.client import Cloudant
-from cloudant.error import CloudantException
+import os, sys
+# from cloudant.client import Cloudant          # deprecated 
+# from cloudant.error import CloudantException  # deprecated
 import requests
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv("../../.env")) 
+
+# Loading packages 
+from ibmcloudant.cloudant_v1 import CloudantV1, Document, AllDocsQuery, IndexField, ViewQuery
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator, ApiException
+
+dct {
+   "URL": os.getenv("URL"),
+   "USERNAME": os.getenv("USERNAME"),
+   "APIKEY": os.getenv("API"),
+}
+
+#  E.g. testting if the dotenv finds the .env file and retrieves creadentials
+print( dct['API'] )
 
 
-def main(param_dict):
-    """Main Function
 
-    Args:
-        param_dict (Dict): input paramater
 
-    Returns:
-        _type_: _description_ TODO
-    """
+# Fundtion of deprecated module  unneccessary
+# def main(param_dict):
+#     try:
+#         client = Cloudant.iam(
+#             account_name=param_dict["COUCH_USERNAME"],
+#             api_key=param_dict["IAM_API_KEY"],
+#             connect=True,
+#         )
+#         print(f"Databases: {client.all_dbs()}")
+#     except CloudantException as cloudant_exception:
+#         print("unable to connect")
+#         return {"error": cloudant_exception}
+#     except (requests.exceptions.RequestException, ConnectionResetError) as err:
+#         print("connection error")
+#         return {"error": err}
 
-    try:
-        client = Cloudant.iam(
-            account_name=param_dict["COUCH_USERNAME"],
-            api_key=param_dict["IAM_API_KEY"],
-            connect=True,
-        )
-        print(f"Databases: {client.all_dbs()}")
-    except CloudantException as cloudant_exception:
-        print("unable to connect")
-        return {"error": cloudant_exception}
-    except (requests.exceptions.RequestException, ConnectionResetError) as err:
-        print("connection error")
-        return {"error": err}
-
-    return {"dbs": client.all_dbs()}
+#     return {"dbs": client.all_dbs()}
